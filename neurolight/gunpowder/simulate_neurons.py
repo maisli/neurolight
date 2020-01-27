4a4a297dfdb6dbf064cd267c740331dec20f4b13
maisli <maisl@c04u01.int.janelia.org>
2020-01-27 11:27:55 -0500

    wip simulate neurons

--- neurolight/gunpowder/simulate_neurons.py
+++ neurolight/gunpowder/simulate_neurons.py
@@ -4,44 +4,37 @@ from gunpowder import *
 from gunpowder.profiling import Timing
 from scipy import ndimage
 from scipy.ndimage.filters import gaussian_filter
-#from skimage import io
-#import h5py
-#from datetime import datetime
+from skimage import io
+import h5py
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
 
 class SimulateNeurons(BatchProvider):
 
-    def __init__(self, raw, raw_spec, gt, gt_spec,
+    def __init__(self, raw, gt, array_specs,
                  size,
                  numinst_min,
                  numinst_max,
-                 points_per_skeleton_min=2,
+                 points_per_skeleton_min=3,
                  points_per_skeleton_max=5,
                  smoothness=1,
                  interpolation="random"
                  ):
         self.raw = raw
         self.gt = gt
+        self.array_specs = array_specs
         self.size = size
         self.numinst_min = numinst_min
         self.numinst_max = numinst_max
-        self.points_per_skeleton_min = max_numinst
-        self.points_per_skeleton_max = max_numinst
+        self.points_per_skeleton_min = points_per_skeleton_min
+        self.points_per_skeleton_max = points_per_skeleton_max
         self.smoothness = smoothness
         self.interpolation = interpolation
-        self.dims = None
-
-        # try to import skelerator
-        try:
-            from skelerator import Tree, Skeleton
-        except ImportError:
-            logger.info("skelerator cannot be imported, please check!")
+        self.dims = 3
 
     def setup(self):
-
-        self.dims = self.spec.get_total_roi().dims()
         offset = Coordinate((0,) * self.dims)
         shape = Coordinate(self.size)
 
@@ -59,12 +52,17 @@ class SimulateNeurons(BatchProvider):
 
         timing = Timing(self)
         timing.start()
-
+        
+        # try to import skelerator
+        try:
+            from skelerator import Tree, Skeleton
+        except ImportError:
+            logger.info("skelerator cannot be imported, please check!")
+        
         batch = Batch()
         n_objects = np.random.randint(self.numinst_min, self.numinst_max + 1)
         raw_spec = request.array_specs[self.raw].copy()
-        shape = raw_spec.roi.get_shape()
-        gt_spec = request.array_specs[self.gt].copy()
+        shape = list(raw_spec.roi.get_shape())
 
         # create noise
         random_noise = np.random.uniform(0, 0.005, [3] + shape)
@@ -86,7 +84,7 @@ class SimulateNeurons(BatchProvider):
                 [np.random.randint(0, shape[i], points_per_skeleton)
                  for i in range(3)], axis=1)
             tree = Tree(points)
-            skeleton = Skeleton(tree, [1, 1, 1], interpolation,
+            skeleton = Skeleton(tree, [1, 1, 1], self.interpolation,
                                 generate_graph=False)
             instance = skeleton.draw(instance, np.array([0, 0, 0]), 1)
             instances.append(instance)
@@ -115,21 +113,24 @@ class SimulateNeurons(BatchProvider):
         for i in range(len(instances)):
             instances[i] *= (i + 1)
 
-        instances = np.concatenate(instances, axis=0).astype(np.uint16)
+        instances = np.stack(instances, axis=0).astype(np.uint16)
         smoothed_noise = smoothed_noise.astype(np.float32)
+        spec = self.array_specs[self.raw].copy()
+        spec.roi = raw_spec.roi.copy()
+        batch[self.raw] = Array(data=smoothed_noise, spec=spec)
 
-        # crop gt
-        instances = instances.crop(gt_spec.roi)
-
-        batch[self.raw] = Array(data=smoothed_noise, spec=raw_spec)
-        batch[self.gt] = Array(data=instances, spec=gt_spec)
+        gt_spec = self.array_specs[self.gt].copy()
+        gt_spec.roi = spec.roi.copy()
+        gt_request = request[self.gt]
+        gt_array = Array(data=instances, spec=gt_spec)
+        batch[self.gt] = gt_array.crop(gt_request.roi)
 
         timing.stop()
         batch.profiling_stats.add(timing)
 
         # write data
         #sample_name = datetime.now().strftime("%d_%b_%Y_%H_%M_%S_%f")
-        #outfn = "/home/maisl/workspace/ppp/flylight/simulated/" + sample_name
+        #outfn = "/nrs/saalfeld/maisl/ppp/simulated/" + sample_name
         #f = h5py.File(outfn + '.hdf', "w")
         #f.create_dataset("instances", data=instances.astype(np.uint16))
         #f.create_dataset("raw", data=smoothed_noise.astype(np.float32))
@@ -137,7 +138,7 @@ class SimulateNeurons(BatchProvider):
 
         #mip = (np.max(smoothed_noise.astype(np.float32), axis=1) * 255).astype(
         #    np.uint8)
-        #io.imsave(outfn + '.png', mip)
+        #io.imsave(outfn + '.png', np.moveaxis(mip, 0, -1))
 
         return batch
 
