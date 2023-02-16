7a82b46367a5306a8ea14dcb89b07b40463920a3
maisli <lisa.mais@gmx.net>
2023-02-16 10:29:05 -0500

    bugfix and add numinst in overlay node

--- neurolight/gunpowder/overlay_augment.py
+++ neurolight/gunpowder/overlay_augment.py
@@ -3,16 +3,21 @@ import numpy as np
 from gunpowder import *
 import os
 import random
+import time
 
 logger = logging.getLogger(__name__)
 
 
 class OverlayAugment(BatchFilter):
 
-    def __init__(self, raw, instances, apply_probability=1.):
+    def __init__(self, raw, instances, apply_probability=1., 
+            overlay_background=False, numinst=None, max_numinst=2):
         self.raw = raw
         self.instances = instances
         self.apply_probability = apply_probability
+        self.overlay_background = overlay_background
+        self.numinst = numinst
+        self.max_numinst = max_numinst
         self.dims = None
 
     def setup(self):
@@ -22,22 +27,26 @@ class OverlayAugment(BatchFilter):
         pass
 
     def process(self, batch, request):
-        if np.sum(batch[self.instances].data > 0) == 0:
-            logger.info("Skipping overlay augment as batch is background batch")
-            return
+        if not self.overlay_background:
+            if np.sum(batch[self.instances].data > 0) == 0:
+                logger.info("Skipping overlay augment as batch is background batch")
+                return
 
         if random.random() < self.apply_probability:
-
+            request._random_seed = int(time.time() * 1e6)
             # get current volume data
-            raw = batch[self.raw].data
-            instances = batch[self.instances].data
+            raw = batch[self.raw].data.copy()
+            instances = batch[self.instances].data.copy()
+            if self.numinst:
+                numinst = batch[self.numinst].data.copy()
 
             # get second volume to overlay with
-            to_overlay = np.random.choice(self.get_upstream_providers(),
-                                          replace=False).request_batch(request)
+            to_overlay = self.get_upstream_provider().request_batch(request)
+            
             to_overlay_raw = to_overlay[self.raw].data
             to_overlay_instances = to_overlay[self.instances].data
 
+            # simply add both volumes together
             overlayed_raw = raw + to_overlay_raw
             overlayed_raw = np.clip(overlayed_raw, 0.0, 1.0)
             overlayed_raw /= float(min(np.max(overlayed_raw), 1.0))
@@ -47,4 +56,10 @@ class OverlayAugment(BatchFilter):
 
             batch[self.raw].data = overlayed_raw.copy()
             batch[self.instances].data = overlayed_instances.copy()
+
+            if self.numinst:
+                to_overlay_numinst = to_overlay[self.numinst].data
+                overlayed_numinst = np.clip(
+                        numinst + to_overlay_numinst, 0, self.max_numinst)
+                batch[self.numinst].data = overlayed_numinst.copy()
 
